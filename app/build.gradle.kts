e45ad0ea181e413b0f7c154218d9c58d81c7fb82
yui <yuineor@gmail.com>
2024-11-20 10:13:46 +0700

    add dependence for recycleView and create fragment_crime_list.xml with it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.lifecycle.extensions)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
