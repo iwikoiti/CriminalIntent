74d2e3ce2a60daf4376b8dc3fa1051d4912634e9
yui <yuineor@gmail.com>
2024-11-21 00:37:25 +0700

    add typeConverters

---
+++ app/src/main/java/com/example/criminalintent/database/CrimeTypeConverters.kt
@@ -0,0 +1,24 @@
+package com.example.criminalintent.database
+
+import androidx.room.TypeConverter
+import java.util.Date
+import java.util.UUID
+
+class CrimeTypeConverters {
+    @TypeConverter
+    fun fromDate(date:Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun toDate(millisSinceEpoch: Long?): Date?{
+        return millisSinceEpoch?.let{
+            Date(it)
+        }
+    }
+
+    @TypeConverter
+    fun fromUUID(uuid: UUID?): String?{
+        return uuid?.toString()
+    }
+}
