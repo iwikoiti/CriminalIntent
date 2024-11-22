254f4cb1dcaf79130498a0444543b1848d10c4a8
yui <yuineor@gmail.com>
2024-11-22 13:06:18 +0700

    access to datas with Dao

--- app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -6,5 +6,6 @@ import com.example.criminalintent.Crime
 
 @Database(entities = [Crime::class], version = 1)
 @TypeConverters(CrimeTypeConverters::class)
-    abstract class CrimeDatabase:RoomDatabase() {
+abstract class CrimeDatabase:RoomDatabase() {
+    abstract fun crimeDao():CrimeDao
 }
